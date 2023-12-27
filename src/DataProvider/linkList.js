dae56a2917129fa741c5bf759a5d5869421e33bb
Shashanka398 <64522691+Shashanka398@users.noreply.github.com>
2023-12-27 23:09:03 +0530

    Update linkList.js

--- src/DataProvider/linkList.js
+++ src/DataProvider/linkList.js
@@ -1,5 +1,5 @@
 export const links={
-        profile:"https://shashank-react-portfolio.vercel.app/",
+        profile:"https://www.linkedin.com/in/shashank-b-721545179/",
         resume:"https://ivory-annalee-74.tiiny.site/",
         github:"https://github.com/Shashanka398",
         whatsApp:"https://wa.me/916362831192"
